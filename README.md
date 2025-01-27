64952eff974d04e2dc6947d1dff0aa4ed2aabf83
floppyhammer <tannhauser_chen@outlook.com>
2025-01-27 10:49:01 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Real Time Video Stabilization
+
+This is a basic implementation of a real time video stabilization algorithm based on [the paper](http://www.sciencedirect.com/science/article/pii/S1877050916314624).
---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include "video_stab.h"
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+int main(int argc, char **argv) {
+    VideoStab stab;
+
+    cv::VideoCapture cap(0);
+
+    cv::Mat prev_frame, cur_frame;
+
+    cap >> prev_frame;
+
+    while (true) {
+        cap >> cur_frame;
+
+        if (cur_frame.data == nullptr) {
+            break;
+        }
+
+        cv::Mat smoothedFrame = stab.stabilize(prev_frame, cur_frame);
+
+        prev_frame = cur_frame.clone();
+    }
+
+    return 0;
+}
